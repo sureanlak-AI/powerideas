2b18a7ce52fc4d0ba76e65c6c1f63b50e1f6451c
Sure anlak <sureanlak@Mac-mini-de-Sure.local>
2026-04-02 00:00:39 +0200

    🧒 Fix: add basename to BrowserRouter for GitHub Pages subpath

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/powerideas">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/article/:id" element={<Article />} />
